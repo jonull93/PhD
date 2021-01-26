1907a84cc1770a68ae59f21be174d2d16c8bc08f
Jonathan Ullmark <jonull@chalmers.se>
2021-01-26 12:14:38 +0100

    fixed crawl arguments

--- rungams.py
+++ rungams.py
@@ -4,6 +4,7 @@ import os
 import random
 import threading
 import time as tm
+import previousInvestments
 from queue import Queue
 
 from gams import *
@@ -20,6 +21,7 @@ starttime = {0: 0.}
 errors = 0
 
 # Create dictionary of scenario-code
+years = []
 regions = ["SE2", "HU", "ES3", "IE"]  # ["SE2","HU","ES3","IE"]
 modes = ["pre", "OR", "OR_inertia", "inertia", "inertia_noSyn"]
 # ["pre", "OR","OR_inertia", "inertia","inertia_noSyn"]
@@ -34,7 +36,7 @@ for mode in modes:
     for region in regions:
         scenarios[region + "_" + mode + ""] = \
             f"""
-$setglobal tot_opt "{region}_{mode}{"_3h" if "_3h" in mode else ""}" 
+$setglobal tot_opt "{region}_{mode}{"_3h" if "_3h" in mode else ""}"
 $setglobal ireg {region}
 $setglobal flexlim yes
 $setglobal startuptime yes
@@ -56,10 +58,13 @@ $setglobal profiling yes
 * Temporal resolution 1, 3 or 6 h
 $setglobal hour_resolution {3 if "_3h" in mode else 1}"""  # [NEEDS TO BE EDITED WHEN SETTING SCRIPT UP]
 
-num_threads = min(8,
-                  len(scenarios))  # should probably be equal or half of your nr of cores (or nr of scenarios, whichever is lower)
+num_threads = min(8, len(scenarios))
+# The "optimal" number of threads depends on your hardware and model
+# but nr of cores /2 seems good unless you hit RAM limit
 
 print("Number of scenarios:", len(scenarios))
+multipleYears = len(years) > 1
+if multipleYears: print("  Multiple years detected, will run previousInvestments.py in-between runs")
 
 # create .inc file for each scenario
 for scen in scenarios:
@@ -98,20 +103,26 @@ def crawl(q, ws, io_lock):
 def run_scenario(workspace, io_lock, scen):
     starttime[scen[0]] = tm.time()
     job = workspace.add_job_from_file(gmsfile)
-    randint = random.randrange(99999999999)  # we create a scenario-specific options file to avoid reading the wrong options file
+    # we create a scenario-specific options file to avoid reading the wrong options file
+    randint = random.randrange(99999999999)
     f = open(path + "options_" + str(randint) + ".txt", "w")
     f.write("LogOption 2\nLogFile " + scen[1] + ".log\nparallelmode -1")
     f.close()
     opt = workspace.add_options(opt_file=path + "options_" + str(randint) + ".txt")
+    # then remove the scenario-specific options file since its not interesting
     os.remove(
-        path + "options_" + str(randint) + ".txt")  # then remove the scenario-specific options file since its not interesting
-    opt.defines["scenariofile"] = scen[1]  # give gams the variable 'scenarioname' with value scen[1]
+        path + "options_" + str(randint) + ".txt")
+    # give gams the variable 'scenarioname' with value scen[1] which is the string
+    opt.defines["scenariofile"] = scen[1]
     print(f" --- Starting scenario {scen[0]}: {scen[1]} in thread", thread_nr[threading.get_ident()], "at",
           dt.datetime.now().strftime('%H:%M:%S'), "---")
     job.run(opt, create_out_db=False)
-    io_lock.acquire()  # we need to make the ouput a critical section to avoid messed up report informations
+    io_lock.acquire()  # we need to make the ouput a critical section to avoid messed up report information
     print("Thread", thread_nr[threading.get_ident()], "finished scenario", scen[1], "(#" + str(scen[0]) + ") at",
           dt.datetime.now().strftime('%H:%M:%S'))
+    if multipleYears and [i for i in scen[1].replace('.', '_').split('_') if "20" in i][0] > years[0]:
+        previousInvestments.doItAll(path, scen[1])  # create previousInvestments.gdx
+
     try:  # these things require job.run to NOT have create_out_db=False
         if job.out_db["ms"][()].value <= 2 and job.out_db["ss"][()].value <= 2:
             print("  Model- and solvestatus OK!")
@@ -133,7 +144,7 @@ thread_nr = {}
 # Starting worker threads on queue processing
 for i in range(num_threads):
     print('Starting thread', i + 1)
-    worker = threading.Thread(target=crawl, args=(q, ws, cp, io_lock))
+    worker = threading.Thread(target=crawl, args=(q, ws, io_lock))
     worker.setDaemon(True)  # setting threads as "daemon" allows main program to
     # exit eventually even if these dont finish
     # correctly.
