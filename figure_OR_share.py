f075165b38f17f6ec59c254788e2e2ef8f3d1911
Jonathan Ullmark <jonull@chalmers.se>
2021-10-20 17:18:59 +0200

    OR_share is now more abstract and allows for other indicators than FR_value_share

--- figure_OR_share.py
+++ figure_OR_share.py
@@ -1,50 +1,57 @@
-import pandas as pd
 import pickle
+from os import path
+
 import matplotlib.pyplot as plt
+import pandas as pd
 
-# ---- input
-# takes manual or pickled data and turns it into DataFrame
-#           2020    2025    ..
-# thermal   0.1     0.05
-# VRE       0.6     0.8
-# ..
-#
-# optionally give series for a line on secondary y axis
 
-region = "brit"
+def percent_stacked_area(region, mode, timestep, indicator_string: str, set: dict, years=None, FC=True, secondary_y="FR_cost"):
+    if years is None:
+        years = [2020, 2025, 2030, 2040]
+    data = pickle.load(open(path.relpath(rf"PickleJar\data_results_{timestep}h.pickle"), "rb"))
+    scenarios = [f"{region}_{mode}_{'fullFC' if FC else 'noFC'}_{y}_{timestep}h" for y in years]
+    if indicator_string == "FR_cost":
+        indicator_data = {pretty: [0. for i in range(len(scenarios))] for pretty in set}
+        for pretty, name in set.items():
+            for i, scen in enumerate(scenarios):
+                try: indicator_data[pretty][i] = data[scen]["FR_cost"].sum(axis=1).sum(level=1)[str(name)]
+                except KeyError: indicator_data[pretty][i] = 0
+                except ValueError:
+                    print("!! weird formatting in FR_cost !!")
+                    try: indicator_data[pretty][i] = data[scen]["FR_cost"].sum(level=2)[str(name)]
+                    except Exception as e: print(type(e),e)
+    else:
+        indicator_data = {pretty: [data[scen][f"{indicator_string}{name}"] for scen in scenarios] for pretty, name in set.items()}
+    secondary_y_values = [data[scen][secondary_y].sum().sum().round()/1000 for scen in scenarios]
+    df = pd.DataFrame(data=
+                      {pretty_name: indicator_data[pretty_name] for pretty_name in indicator_data}
+                      , index=years).round(decimals=4)
+    print(df)
+    print(f"{secondary_y}: {secondary_y_values}")
+
+    # ---- plotting
+    # takes DataFrame and plots stacked area and optionally a line on secondary y axis
+    ax = df.div(df.sum(axis=1), axis=0).plot(kind="area", linewidth=0)
+    ax.set_xticks(df.index)
+    ax.set_title(f"Reserve share and cost, {region}, {mode}")
+    ax.set_ylabel("Share [-]")
+    ax2 = ax.twinx()
+    ax2.set_ylabel("Total reserve cost per year [M€]")
+    ax2.plot(df.index, secondary_y_values, "k--")
+    ax2.set_ylim(ymin=0)
+    # ax2.ticklabel_format(style="sci", scilimits=(0, 0))
+    return ax, ax2
+
+
 mode = "lowFlex"
 timestep = 12
-data = pickle.load(open(rf"C:\Users\jonull\git\python\PickleJar\data_results_{timestep}h.pickle", "rb"))
-years = [2020, 2025, 2030, 2040]
-scenarios = [f"{region}_{mode}_fullFC_{y}_{timestep}h" for y in years]
-thermal = [data[scen]["OR_value_share_thermal"] for scen in scenarios]  # [0.45, 0.3, 0.2, 0.15]
-print(thermal)
-VRE = [data[scen]["OR_value_share_VRE"] for scen in scenarios]  # [0.5, 0.1, 0.1, 0.05]
-ESS = [data[scen]["OR_value_share_ESS"] for scen in scenarios]  # [0, 0.5, 0.6, 0.7]
-BEV = [data[scen]["OR_value_share_BEV"] for scen in scenarios]  # [0.001, 0, 0, 0]
-PtH = [data[scen]["OR_value_share_PtH"] for scen in scenarios]
-hydro = [data[scen]["OR_value_share_hydro"] for scen in scenarios]  # [0.05, 0.1, 0.1, 0.1]
+reserve_technologies = {"Thermals": "thermal", "VRE": "VRE", "ESS": "ESS", "BEV": "BEV", "PtH": "PtH", "Hydro": "hydro"}
+FR_intervals = {"FFR": 1, "5-30s": 2, "30s-5min": 3, "5-15min": 4, "15-30min": 5, "30-60min": 6}
 
-FR_price = [0, 0, 0, 0]
-df = pd.DataFrame(data=
-                  {"Thermals": thermal,
-                   "VRE": VRE,
-                   "ESS": ESS,
-                   "BEV": BEV,
-                   "PtH": PtH,
-                   "Hydro": hydro
-                   }, index=[2020, 2025, 2030, 2040]).round(decimals=4)
-print(df)
-
-# ---- plotting
-# takes DataFrame and plots stacked area and optionally a line on secondary y axis
-ax = df.plot(kind="area", linewidth=0)
-ax.set_xticks(df.index)
-ax.set_title(f"Reserve share and cost, {region}, {mode}")
-ax.set_ylabel("Reserve share per technology")
-ax2 = ax.twinx()
-ax2.set_ylabel("Total reserve cost each year")
-ax2.plot(df.index, FR_price, "k--")
-ax2.set_ylim(ymin=0)
-#plt.show()
-plt.savefig(rf"figures\reserve_share_{region}_{mode}.png")
+for region in ["nordic", "brit", "iberia"]:
+    print("_______________"*3)
+    print(f" .. Making figure for {region} .. ")
+    ax = percent_stacked_area(region, mode, timestep, "FR_value_share_", reserve_technologies)
+    plt.savefig(rf"figures\reserve_share_{region}_{mode}.png")
+    ax = percent_stacked_area(region, mode, timestep, "FR_cost", FR_intervals)
+    plt.savefig(rf"figures\interval_costs_{region}_{mode}.png")
