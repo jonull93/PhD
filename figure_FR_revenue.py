c6bce5ce41b0dcd87afca150abb79b389d2ed99f
Jonathan Ullmark <jonathan.ullmark@hotmail.com>
2022-07-05 16:17:35 +0200

    start of new fig script

--- figure_FR_revenue.py
+++ figure_FR_revenue.py
@@ -3,9 +3,8 @@ import os
 import pickle
 import order_cap
 import pandas as pd
-from my_utils import print_red, print_green, print_cyan
-
-# os.chdir(r"C:\Users\Jonathan\Box\python")  # not needed unless running line-by-line in a console
+import matplotlib.pyplot as plt
+from my_utils import color_dict, order_cap, add_in_dict, tech_names, scen_names, print_cyan, print_red, print_green, year_names
 
 file_suffix = ""
 if len(file_suffix) > 0: file_suffix = "_" + file_suffix
@@ -14,34 +13,31 @@ if len(scen_suffix) > 0: scen_suffix = "_" + scen_suffix
 timestep = 6
 data = pickle.load(open(os.path.relpath(rf"PickleJar\data_results_{timestep}h{file_suffix}.pickle"), "rb"))
 
-size = {}
-for key in data:
-    #print(f"{key} takes up {sys.getsizeof(data[key])} bytes")
-    for key2 in data[key]:
-        #print(f"{key2} takes up {sys.getsizeof(data[key][key2])} bytes")
-        size[key2] = sys.getsizeof(data[key][key2])
-    break
-
-size_s = pd.Series(data=size.values(), index=size.keys())
-#print(size_s.sort_values(ascending=False)[:10])
-
 total_rev = {}
 FR_rev = {}
-for region in ["nordic", "brit", "iberia"]:
-    for flex in ["lowFlex", "highFlex"]:
+FR_share = {}
+scenarios = []
+fig, axs = plt.subplots(nrows=3, ncols=2, figsize=(6, 7))
+for i_r, region in enumerate(["nordic", "brit", "iberia"]):
+    for i_f, flex in enumerate(["lowFlex", "highFlex"]):
         for FC in ["noFC", "fullFC"]:
             for year in [2020, 2025, 2030, 2040]:
+                # Gathering needed data
                 scen = f"{region}_{flex}_{FC}_{year}_{timestep}h"
+                scenarios.append(scen)
                 print_cyan(scen)
                 #scen = "nordic_lowFlex_fullFC_2025_6h"
                 total_rev[scen] = data[scen]["tech_revenue"]
                 PtH = ["EB", "HP"]
                 if "fullFC" in FC:
                     FR_rev[scen] = data[scen]["tech_revenue_FR"]
+                    FR_share[scen] = FR_rev[scen]/total_rev[scen]
+                    # Plotting the data
+                    FR_rev[scen].plot(ax=axs[i_r,i_f], kind="bar")
+                    plt.show()
                     #inertia_rev = data[scen]["tech_revenue_inertia"]
                     #FC_inertia = FR_rev+inertia_rev
-                    print_green((FR_rev[scen]/(total_rev[scen])).sort_values(ascending=False)[:10])
+                    #print_green((FR_rev[scen]/(total_rev[scen])).sort_values(ascending=False)[:10])
                 #else:
                     #print_red(total_rev[scen].sort_values(ascending=False)[:10])
 
-fig_scen = "nordic_lowFlex_fullFC_2025_6h"
