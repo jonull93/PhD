865913613ff91779a3c86b62532571ed0321ecb6
Jonathan Ullmark <jonathan.ullmark@hotmail.com>
2022-08-18 09:06:45 +0200

    updated boxplot and new dice script

--- testspace.py
+++ testspace.py
@@ -36,18 +36,36 @@ def aggregate_in_df(df, index_list: list, new_index: str):
 
 years = [2020,2025,2030,2040]
 regions = ["nordic", "brit", "iberia"]
+h = 3
+flex = "lowFlex"
 #fig, axes = plt.subplots(nrows=1, ncols=4, figsize=(8, 3))
 FR_demand_6 = pd.DataFrame(columns=pd.MultiIndex(levels=[[],[]],
                       codes=[[],[]],
                       names=['Year','Region']))
 for i_y, year in enumerate(years):
     for region in regions:
-        scen = f"{region}_lowFlex_fullFC_{year}_3h"
+        scen = f"{region}_{flex}_fullFC_{year}_{h}h"
         FR_demand_6[year, region] = data[scen]["FR_demand"]["total"].loc[:, '6', :].sum(axis=0)
     print(FR_demand_6)
 #    plt.sca(axes[i_y])
 #    plt.ylim([0,30])
-plt.boxplot(FR_demand_6)
-plt.xticks(range(1,len(FR_demand_6.columns)+1), FR_demand_6.columns)
+plt.boxplot(FR_demand_6, whis=(1,99), sym="x")
+plt.ylim(0,30)
+axes = plt.gca()
+axes.yaxis.grid()
+xregions = [r.capitalize() for r in regions]*4
+plt.xticks(range(1,len(FR_demand_6.columns)+1), xregions)
+for i in range(len(FR_demand_6.columns)+1): #draw seperating lines between xlabels
+    x = [i+0.5]*2
+    if i%3==0:
+        y = [0, -3.5]
+        plt.plot(x,y,clip_on=False,color="black",linewidth=1)
+        plt.plot(x, axes.get_ylim(), clip_on=False, color="grey", linewidth=0.5, ls=(15,(5,20)))
+        if i<len(FR_demand_6.columns):
+            plt.text(i+2, -3.5, years[int(i/3)], fontsize=12, ha="center")
+plt.ylabel("Frequency reserve demand [GW]")
+plt.title(f"Reserve demand, {flex}")
+plt.tight_layout()
+plt.savefig(rf"figures\FR_demand_boxplot_{h}h.png", dpi=600)
 plt.show()
-print(data[scen]["FR_demand"]["other"].loc[:,'6',:].median(axis=1))
+print(data["brit_lowFlex_fullFC_2030_3h"]["FR_demand"])
