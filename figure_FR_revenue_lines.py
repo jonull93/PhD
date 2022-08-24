059e6ef457e899efb04245ea2c8fc9817e5ce46b
Jonathan Ullmark <jonathan.ullmark@chalmers.se>
2022-08-24 17:29:43 +0200

    Update figure_FR_revenue_lines.py

--- figure_FR_revenue_lines.py
+++ figure_FR_revenue_lines.py
@@ -33,9 +33,9 @@ techs = []
 years = ["2020", "2025", "2030", "2040"]
 regions = ["nordic", "brit", "iberia"]
 flexes = ["lowFlex", "highFlex"]
-technologies_to_show = ["bat","PtH","thermals","Hydro"]
-per_cap = False
-fig, axs = plt.subplots(nrows=len(technologies_to_show), ncols=1, figsize=(7, 6))
+technologies_to_show = ["bat","PtH","thermals","Hydro", "H2store"]
+per_cap = True
+fig, axs = plt.subplots(nrows=len(technologies_to_show), ncols=1, figsize=(6, 7))
 df_rev = {tech: pd.DataFrame(0,pd.MultiIndex.from_product([regions,flexes],names=["Region","Flex"]), years)
                   for tech in technologies_to_show}
 df_share = {flex: pd.DataFrame(0,pd.MultiIndex.from_product([regions,years],names=["Region","Year"]), technologies_to_show)
@@ -48,24 +48,16 @@ for i_y, year in enumerate(years):
                 # Gathering needed data
                 scen = f"{region}_{flex}_{FC}_{year}_{timestep}h"
                 scenarios.append(scen)
-                #print_cyan(scen)
-                #scen = "nordic_lowFlex_fullFC_2025_6h"
                 try:
                     total_rev[scen] = data[scen]["tech_revenue"].sum(level=0)
                     cap[scen] = data[scen]["tot_cap"].sum(level=0)
                 except KeyError as e:
-                    print(e)
+                    print_red(e)
                     continue
                 except AttributeError as e:
-                    print(e)
+                    print_red(e)
                     continue
                 if "fullFC" in FC:
-                    """FR_rev[scen].loc["Base"] = FR_rev[scen].loc[FR_rev[scen].index.intersection(baseload)].sum()
-                    FR_rev[scen].drop(baseload, inplace=True, errors='ignore')
-                    FR_rev[scen].loc["Peak"] = FR_rev[scen].loc[FR_rev[scen].index.intersection(peak)].sum()
-                    FR_rev[scen].drop(peak, inplace=True, errors='ignore')
-                    FR_rev[scen].loc["CHP"] = FR_rev[scen].loc[FR_rev[scen].index.intersection(CHP)].sum()
-                    FR_rev[scen].drop(CHP, inplace=True, errors='ignore')"""
                     FR_rev[scen] = data[scen]["tech_revenue_FR"].sum(level=0)
                     FR_rev[scen] = aggregate_in_df(FR_rev[scen], wind, "Wind")
                     cap[scen] = aggregate_in_df(cap[scen], wind, "Wind")
@@ -78,16 +70,16 @@ for i_y, year in enumerate(years):
                     cap[scen] = aggregate_in_df(cap[scen], ["RO", "RR"], "Hydro")
                     FR_rev[scen] = aggregate_in_df(FR_rev[scen], PtH, "PtH")
                     cap[scen] = aggregate_in_df(cap[scen], PtH, "PtH")
-                    print(FR_rev[scen])
+#                    print(FR_rev[scen])
                     for tech in df_rev:
                         try:
                             if per_cap:
                                 df_rev[tech].loc[(region,flex),year] = (FR_rev[scen][tech] / cap[scen][tech] / 1000).round(1)
-                                if df_rev[tech].loc[(region,flex),year] > 10:
-                                    print(tech)
-                                    print((FR_rev[scen][tech] / 1000).round(1))
-                                    print(cap[scen][tech])
-                                    print(df_rev[tech].loc[(region,flex),year])
+#                                if df_rev[tech].loc[(region,flex),year] > 10:
+#                                    print(tech)
+#                                    print((FR_rev[scen][tech] / 1000).round(1))
+#                                    print(cap[scen][tech])
+#                                    print(df_rev[tech].loc[(region,flex),year])
                             else:
                                 df_rev[tech].loc[(region, flex), year] = (FR_rev[scen][tech] / 1000).round(1)
                         except KeyError: continue
@@ -108,12 +100,23 @@ for i_y, year in enumerate(years):
                     # print_red(FR_rev[scen])
                     # print_cyan(total_rev[scen])
                     df_share[flex].loc[(region,year)] = (FR_rev[scen]/total_rev[scen]).round(3)*100
-for flex in flexes:
-    print_red(flex)
-    df_share[flex].fillna(0,inplace=True)
-    for region in regions:
-        print(region)
-        print_green(df_share[flex].loc[region])
+
+for region in regions:
+    print_red(region)
+    for flex in flexes:
+        df_share[flex].fillna(0, inplace=True)
+    temp_df = df_share[flexes[0]].loc[region].astype(str)
+    for r in range(temp_df.shape[0]):
+        for c in range(temp_df.shape[1]):
+            lf_val = df_share[flexes[0]].loc[region].iat[r, c]
+            hf_val = df_share[flexes[1]].loc[region].iat[r, c]
+            try: temp_df.iat[r, c] = f"{round(lf_val)} / {round(hf_val)}"
+            except:
+                print_red(r,c)
+                print_red(temp_df)
+                raise
+    print_green('\n'.join(temp_df.astype(str).apply(lambda x: ', '.join(x), axis=1)))
+print(temp_df)
         # Plotting the data
 for i_t, tech in enumerate(technologies_to_show):
     print_cyan(tech)
@@ -124,21 +127,22 @@ for i_t, tech in enumerate(technologies_to_show):
     for i_l, line in enumerate(axs[i_t].get_lines()):
         line.set_marker(markers[i_l])
     for i_r, region in enumerate(regions):
-        axs[i_t].fill_between(years,df_rev[tech].loc[(region,"lowFlex")],df_rev[tech].loc[(region,"highFlex")], alpha=0.3, color=['teal', 'purple', 'orange'][i_r])
+        axs[i_t].fill_between(years,df_rev[tech].loc[(region,"lowFlex")],df_rev[tech].loc[(region,"highFlex")],
+                              alpha=0.3, color=['teal', 'purple', 'orange'][i_r])
     axs[i_t].set_title(tech_names[tech], fontsize=12)
     axs[i_t].set_xticks([0,1,2,3])
     axs[i_t].set_xticklabels([year_names[int(year)].capitalize() for year in years]) #, rotation=20, ha="center", va="top", rotation_mode='anchor'
 if i_r == 0 and False:
     axs[i_r].text(0.1, 0.5, year_names_twolines[year].capitalize(), transform=axs[i_r].transAxes,
                                ha="center", va="center", fontsize=12)
-plt.figtext(0,0.5,f"Revenue{' per capacity' if per_cap else ''} [M€/{'GW/' if per_cap else ''}yr]", fontsize=12, rotation=90, va="center",ha="center")
+plt.figtext(0,0.5,f"Revenue{' per capacity' if per_cap else ''} [M€/{'GW(h)/' if per_cap else ''}yr]", fontsize=12, rotation=90, va="center",ha="center")
 #axs[1].set_ylabel("Revenue [M€/yr]", fontsize=12)
 
 #plt.figtext(0.21, 0.5, "Revenue from reserves [M€/yr]", ha="center", va="center", rotation=90)
 #plt.figtext(1.02, 0.5, "Reserve share of total revenue [-]", ha="right", va="center", rotation=90)
 lines = [Line2D([0], [0], color=['teal','purple','orange'][i_r], lw=5) for i_r in range(len(regions))]
-legend = fig.legend(lines, ["Nordic+","Brit","Iberia"], loc="lower center", bbox_transform=axs[0].transAxes, bbox_to_anchor=(0.5, 1.27), ncol=3)
-fig.suptitle(f"Reserve supply revenue{' per installed capacity' if per_cap else ''}", y=1.02, fontsize=13)
+legend = fig.legend(lines, ["Nordic+","Brit","Iberia"], loc="lower center", bbox_transform=axs[0].transAxes, bbox_to_anchor=(0.5, 1.29), ncol=3)
+fig.suptitle(f"Reserve supply revenue{' per installed capacity' if per_cap else ''}", y=1.017, fontsize=13)
 plt.tight_layout()
 fig_path = f"figures\\"
 plt.savefig(fig_path+f"FR_revenue_lines_{timestep}h{'_per_cap' if per_cap else ''}.png", dpi=400, bbox_inches="tight")
