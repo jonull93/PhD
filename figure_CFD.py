1161a7d68289e166831e7de2b30b1c07efc16587
Jonathan Ullmark @8 <jonathan.ullmark@chalmers.se>
2023-03-23 12:41:40 +0100

    more functions and starting plot combinations

--- figure_CFD.py
+++ figure_CFD.py
@@ -126,73 +126,105 @@ def fast_cfd(df_netload, xmin, xmax, amp_length=0.1, area_method=False):
     return df_out_tot
 
 
+def create_df_out_tot(year, xmin, xmax):
+    data = pickle.load(open(f"PickleJar\\netload_components_{year}.pickle", "rb"))
+    VRE_profiles = data["VRE_profiles"]
+    load = data["load"]
+    cap = data["cap"]
+    if type(load) == dict:
+        load_list = []
+        for _year, load in load.items():
+            load_list += list(load)
+        load = np.array(load_list)
+    if load.ndim > 1:
+        load = load.sum(axis=1)
+    net_load = -(VRE_profiles * cap).sum(axis=1) + load
+    # print(VRE_profiles.shape, net_load.shape, cap.shape)
+    # d = {'net load': net_load,'count1':0,'count2':0}
+    # df_netload = fast_rolling_average(pd.DataFrame(data=d),1)
+    array_netload = fast_rolling_average(net_load, rolling_hours)
+    df_netload = pd.DataFrame(data={'net load': array_netload, 'count1': 0, 'count2': 0})
+    xmax = max(xmax, int(math.ceil(df_netload["net load"].max())))
+    xmin = min(xmin, int(math.floor(df_netload["net load"].min())))
+    df_out_tot = fast_cfd(df_netload, xmin, xmax, amp_length=amp_length, area_method=area_mode_in_cfd)
+    return df_out_tot, xmin, xmax
+
+
+def make_cfd_plot(ax, Xnetload, Ynetload, Znetload, year, rolling_hours, xmin=False, xmax=False, ymin=False, ymax=False):
+    ax.pcolormesh(Xnetload, Ynetload, Znetload, alpha=1, linewidth=0, shading='nearest',
+                   cmap=plt.cm.turbo)  # , alpha=0.7)
+    if xmin and xmax:
+        ax.set_xlim([xmin, xmax])
+    if ymin and ymax:
+        ax.set_ylim([ymin, ymax])
+    from matplotlib.ticker import (MultipleLocator, AutoMinorLocator)
+    ax.xaxis.set_minor_locator(AutoMinorLocator())
+    ax.yaxis.set_minor_locator(AutoMinorLocator())
+    ax.grid(visible=True, color="black", lw=1, axis="both", alpha=0.15, which="both")
+    ax.set_xlabel("Amplitude [GW]")
+    ax.set_ylabel("Duration [days]")
+    ax.set_title(f"Amplitude-Duration-Recurrence for {year}, {rolling_hours}h window")
+    return ax
+
 def main(year, amp_length=1, rolling_hours=12, area_mode_in_cfd=True, write_files=True, read_pickle=True, xmin=0, xmax=0, ymin=0, ymax=0, weights=False):
     if type(year) != list and type(year) != tuple:
         print_cyan(f"\nStarting loop for year -- {year} --")
         pickle_read_name = rf"PickleJar\{year}_CFD_netload_df_amp{amp_length}_window{rolling_hours}{'_area'*area_mode_in_cfd}.pickle"
         pickle_dump_name = rf"PickleJar\{year}_CFD_netload_df_amp{amp_length}_window{rolling_hours}{'_area'*area_mode_in_cfd}.pickle"
-
         # df_netload = df_netload.reset_index()[["net load", "count1", "count2"]]
         try:
             if not read_pickle: raise ImportError
             df_out_tot = pickle.load(open(pickle_read_name, "rb"))
         except Exception as e:
             if read_pickle: print_red(f"Failed to read {pickle_read_name} due to {type(e)}")
-            data = pickle.load(open(f"PickleJar\\netload_components_{year}.pickle", "rb"))
-            VRE_profiles = data["VRE_profiles"]
-            load = data["load"]
-            cap = data["cap"]
-            if type(load) == dict:
-                load_list = []
-                for _year, load in load.items():
-                    load_list += list(load)
-                load = np.array(load_list)
-            if load.ndim > 1:
-                load = load.sum(axis=1)
-            net_load = -(VRE_profiles * cap).sum(axis=1) + load
-            # print(VRE_profiles.shape, net_load.shape, cap.shape)
-            # d = {'net load': net_load,'count1':0,'count2':0}
-            # df_netload = fast_rolling_average(pd.DataFrame(data=d),1)
-            array_netload = fast_rolling_average(net_load, rolling_hours)
-            df_netload = pd.DataFrame(data={'net load': array_netload, 'count1': 0, 'count2': 0})
-            xmax = max(xmax, int(math.ceil(df_netload["net load"].max())))
-            xmin = min(xmin, int(math.floor(df_netload["net load"].min())))
             start_time = timer()
-            df_out_tot = fast_cfd(df_netload, xmin, xmax, amp_length=amp_length, area_method=area_mode_in_cfd)
+            df_out_tot, xmin, xmax = create_df_out_tot(year, xmin, xmax)
             # 248s at 1 year then more changes and now 156-157s at 1 year
             end_time = timer()
             print(f"elapsed time to build CFD in thread {thread_nr[threading.get_ident()]} = {round(end_time - start_time, 1)}")
             if write_files: pickle.dump(df_out_tot, open(pickle_dump_name, 'wb'))
-        #print(df_out_tot.iloc[:40])
-        #print(df_out_tot)
+        # print(df_out_tot.iloc[:40])
+        # print(df_out_tot)
         df_reset = df_out_tot.reset_index()
         df_reset.columns = ['Amplitude', 'Duration', 'Occurrence']
         xmax = max(xmax, int(math.ceil(df_reset["Amplitude"].max())))
         xmin = min(xmin, int(math.floor(df_reset["Amplitude"].min())))
         df_pivot = df_reset.pivot(index='Amplitude', columns='Duration')
-        filtered_df = df_reset[df_reset['Amplitude'].round(1) == 25.5]
-        #print("Filtered df =", filtered_df)
+        # filtered_df = df_reset[df_reset['Amplitude'].round(1) == 25.5]
+        # print("Filtered df =", filtered_df)
         # df_reset["Energy"] = df_reset["Amplitude"]*df_reset["Duration"]*np.sign(df_reset["Amplitude"])
         # unique_amps, unique_amps_index = np.unique(df_reset["Amplitude"],return_index=True)
         # print(df_pivot[df_pivot.columns[df_pivot.columns.get_level_values(1) > 375]].to_string())
         # print(df_pivot[df_pivot.columns[df_pivot.columns.get_level_values(1) > 1300]].fillna(0)[df_pivot != 0])
         # print(df_pivot[df_pivot["Duration"] >1300].fillna(0).sum())
-        Y = df_pivot.columns.levels[1].values/24
+        Y = df_pivot.columns.levels[1].values/24  # convert y axis values to days
         ymin = min(ymin, Y.min())
         ymax = max(ymax, Y.max())
         X = df_pivot.index.values
         Z = df_pivot.values
-        #print_cyan("Y =", Y, Y.shape)
+        # print_cyan("Y =", Y, Y.shape)
         # print_green("X =", X, X.shape)
-        #print_red("Z =", Z)
+        # print_red("Z =", Z)
         Ynetload, Xnetload = np.meshgrid(Y, X)
         if write_files:
             scipy.io.savemat(f"output\\heatmap_values_{year}_amp{amp_length}_window{rolling_hours}{'_area'*area_mode_in_cfd}.mat",
                          {"amplitude": Ynetload, "duration": Xnetload, "recurrance": Z})
-    else:
+        if year == "1980-2019":
+            Z = Z / 40
+        Znetload = np.where(Z > 50, 50, Z)
+        Znetload = np.where(Znetload == 0., np.nan, Znetload)
+        fig, ax = plt.subplots()
+        make_cfd_plot(ax, Xnetload, Ynetload, Znetload, year, rolling_hours, xmin, xmax, ymin, ymax)
+        fig.tight_layout()
+        filename = f"figures\\profile_analysis\\cfd_{year}_amp{amp_length}_window{rolling_hours}{'_area' * area_mode_in_cfd}.png"
+        fig.savefig(filename, dpi=500)
+        plt.close(fig)
+
+    else:  # this is the loop for multiple years from fingerprintmatching.jl
         print_cyan(f"\nStarting loop for years -- {year} --")
+        year, weights = year
         if len(year) != len(weights):
-            raise ValueError("year and weights must have the same length")
+            raise ValueError("years and weights must have the same length")
         m = {}
         Zs = {}
         for y in year:
@@ -204,35 +236,27 @@ def main(year, amp_length=1, rolling_hours=12, area_mode_in_cfd=True, write_file
                 Xnetload = np.array(f["duration"])
                 Zs[y] = np.array(f["recurrance"])
             #m[y] = scipy.io.loadmat(f"output\\heatmap_values_{y}_amp{amp_length}_window{rolling_hours}{'_area'*area_mode_in_cfd}_padded.mat")
+        # load the reference Znetload from 1980-2019
+        with h5py.File(
+                f"output/heatmap_values_1980-2019_amp{amp_length}_window{rolling_hours}{'_area' * area_mode_in_cfd}_padded.mat",
+                "r") as f:
+            Znetload_ref = np.array(f["recurrance"])
+
         # construct Y_netload, Xnetload and Z using m and the weights
-        #Ynetload = sum([Ynetloads[y] * w for y, w in zip(year, weights)])
-        #Xnetload = sum([m[y]["duration"][:] * w for y, w in zip(year, weights)])
         Z = sum([Zs[y] * w for y, w in zip(year, weights)])
-    if year == "1980-2019":
-        Z = Z/40
-    Znetload = np.where(Z > 50, 50, Z)
-    Znetload = np.where(Znetload == 0., np.nan, Znetload)
-    print(f"{Z = }")
+        Znetload = np.where(Z > 50, 50, Z)
+        Znetload = np.where(Znetload == 0., np.nan, Znetload)
+        fig, axes = plt.subplots(1, 2, figsize=(8, 5))
+        make_cfd_plot(axes[0], Xnetload, Ynetload, Znetload, year, rolling_hours, xmin, xmax, ymin, ymax)
+        Znetload_diff =
+
+    # print(f"{Z = }")
     # print({"amplitude":Xnetload, "duration":Ynetload, "recurrance":Znetload})
-    #Z_testing = np.nan_to_num(Znetload)
-    #print(Z_testing.sum(axis=0), Z_testing.sum(axis=0).shape)
+    # Z_testing = np.nan_to_num(Znetload)
+    # print(Z_testing.sum(axis=0), Z_testing.sum(axis=0).shape)
     # import matplotlib as mpl
     # mpl.rcParams["patch.force_edgecolor"]=True
-    fig, ax = plt.subplots()
-    ax.pcolormesh(Xnetload, Ynetload, Znetload, alpha=1, linewidth=0, shading='nearest',
-                   cmap=plt.cm.turbo)  # , alpha=0.7)
-    ax.set_xlim([xmin, xmax])
-    ax.set_ylim([ymin, ymax])
-    from matplotlib.ticker import (MultipleLocator, AutoMinorLocator)
-    ax.xaxis.set_minor_locator(AutoMinorLocator())
-    ax.yaxis.set_minor_locator(AutoMinorLocator())
-    ax.grid(visible=True, color="black", lw=1, axis="both", alpha=0.15, which="both")
-    ax.set_xlabel("Amplitude [GW]")
-    ax.set_ylabel("Duration [days]")
-    ax.set_title(f"Amplitude-Duration-Recurrence for {year}, {rolling_hours}h window")
-    fig.tight_layout()
-    fig.savefig(f"figures\\profile_analysis\\cfd_{year}_amp{amp_length}_window{rolling_hours}{'_area'*area_mode_in_cfd}.png", dpi=500)
-    plt.close(fig)
+
     #plt.show()
     return xmin, xmax, ymin, ymax
 
@@ -270,7 +294,7 @@ xmin, xmax, ymin, ymax = main(long_period, amp_length=amp_length, rolling_hours=
 print("Xmin =", xmin, "Xmax =", xmax, "Ymin =", ymin, "Ymax =", ymax)
 queue_years = Queue(maxsize=0)
 # Load results from most recent fingerprinting run
-with open("results/most_recent_results", "r") as f:
+with open("results/most_recent_results.txt", "r") as f:
     folder_name = f.read().strip()
 
 import json
@@ -278,10 +302,18 @@ import json
 with open(f"{folder_name}/results.json", "r") as f:
     results = json.load(f)
 
+print(results.keys())
 combinations = results["combinations"]
-weights = results["weights"]
-
-combined_results = [(combinations[i], weights[i]) for i in range(len(combinations))]
+combinations_strings = [str(comb).replace("'",'"') for comb in combinations]
+weights = results["best_weights"]
+#print(weights.keys())
+#print(combinations)
+for comb in combinations_strings:
+    key = comb
+    try: print(comb, [round(weights[key][i], 3) for i in range(len(weights[key]))])
+    except KeyError: print_red(f"KeyError for {key} ")
+combined_results = [(comb, [round(weights[key][i], 3) for i in range(len(weights[key]))]) for key, comb in zip(combinations_strings, combinations)]
+print(combined_results)
 
 for i in combined_results:
     queue_years.put(i)
