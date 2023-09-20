25972d2b310e7690fecf86f832031c6aada89d8f
Xhaf <jonathan.ullmark@hotmail.com>
2023-09-20 16:03:23 +0200

    cleaned up scripts a bit an added halfstep support

--- halfstepped_ref_sheet.py
+++ halfstepped_ref_sheet.py
@@ -1,36 +1,91 @@
 import pandas as pd
+from my_utils import print_green, print_red
 
-# Load data from the Excel file
-filename = r'input\cap_ref.xlsx'
-sheet1 = pd.read_excel(filename, sheet_name='ref134')
-sheet2 = pd.read_excel(filename, sheet_name='ref133')
+def load_data(ref1, ref2=False, ref3=False, filename = r'input\cap_ref.xlsx'):
+    # Load data from the Excel file
+    if ref2==False:
+        id = int(ref1[3:])
+        ref2 = f"ref{id-1}"
+    sheet1 = pd.read_excel(filename, sheet_name=ref1)
+    sheet2 = pd.read_excel(filename, sheet_name=ref2)
+    if ref3:
+        sheet3 = pd.read_excel(filename, sheet_name=ref3)
+        sheet2 = pd.concat([sheet2, sheet3])
+    # Combine both sheets
+    combined = pd.concat([sheet1, sheet2])
+    return combined
 
-# Combine both sheets
-combined = pd.concat([sheet1, sheet2])
 
-# Convert the data into a multi-indexed series and compute the average
-avg_capacity = combined.groupby(['tech', 'I_reg'])['capacity'].mean()
+def calculate_mean_cap(data):
+    # Convert the data into a multi-indexed series and compute the average
+    avg_capacity = data.groupby(['tech', 'I_reg'])['capacity'].mean()
+    # Prepare the DataFrame to be written back to Excel
+    result = avg_capacity.sort_values(ascending=False).reset_index()
+    return result
 
-# Prepare the DataFrame to be written back to Excel
-result = avg_capacity.reset_index()
 
-# Load the entire Excel file
-with pd.ExcelWriter(filename, engine='openpyxl', mode='a') as writer:
-    # Check if "info" and "cap_comp" sheets exist
-    if 'info' in writer.sheets:
-        info_position = writer.sheets.index('info')
+def append_to_capref(data, my_sheetname, filename = r'input\cap_ref.xlsx'):
+    # First, check if the Excel file exists and is not already open
+    try:
+        with open(filename, 'a') as f:
+            pass
+    except IOError:
+        print_red(f"!! The file {filename} does not exist. Please doublecheck the path.")
+        return
+    except PermissionError:
+        print_red(f"!! The file {filename} is already open. Please close it and then type something below:")
+        input("Press Enter to continue...")
+    # Check if my_sheetname already exists in the Excel file and whether it is empty
+    sheets = pd.ExcelFile(filename).sheet_names
+    if my_sheetname in sheets:
+        # if it is empty, delete it and continue
+        if pd.read_excel(filename, sheet_name=my_sheetname).empty:
+            print_green(f"Sheet {my_sheetname} already exists but is empty, so it will be deleted.")
+            with pd.ExcelWriter(filename, engine='openpyxl', mode='a') as writer:
+                writer.book.remove(writer.book[my_sheetname])
+        # if it is not empty, leave it be and exit
+        else:
+            print_red(f"!! Sheet {my_sheetname} already exists and is not empty, so it will not be modified.")
+            return
+    # Load the entire Excel file
+    with pd.ExcelWriter(filename, engine='openpyxl', mode='a') as writer:
+        # Write the result DataFrame
+        data.to_excel(writer, sheet_name=my_sheetname, index=False)
+        # Position 'averaged_data' in the third place
+        workbook = writer.book
+        sheet = workbook[my_sheetname]
+        idx = workbook._sheets.index(sheet)
+        workbook._sheets.insert(2, workbook._sheets.pop(idx))
+    print_green(f"Added sheet {my_sheetname} to the sheet!")
+
+
+def ask_for_refname():
+    # Ask the user for the ref to use
+    ref1 = input("Enter the ref to use (separated by , if multiple): ")
+    if "," in ref1:
+        refs = ref1.split(",")
+        ref1 = refs[0]
+        ref2 = refs[1]
+        if len(refs) > 2:
+            ref3 = refs[2]
     else:
-        info_position = 0
+        ref2 = False
+        ref3 = False
+    # new ref name will be the digits of ref except the FIRST DIGIT (not whole number) will be incremented by 2
+    # e.g. ref134 -> ref334
+    id_to_keep = int(ref1[4:])
+    id_to_increment = int(ref1[3]) + 2
+    my_sheetname = f"ref{id_to_increment}{id_to_keep}"
     
-    if 'cap_comp' in writer.sheets:
-        cap_comp_position = writer.sheets.index('cap_comp')
-    else:
-        cap_comp_position = 1
-    
-    # Define the desired position for the new sheet
-    start_position = max(info_position, cap_comp_position) + 1
-    
-    # Write the result DataFrame in the desired position
-    result.to_excel(writer, sheet_name='averaged_data', index=False, startrow=start_position)
+    return ref1, ref2, ref3, my_sheetname
 
-print("Done!")
+
+if __name__ == "__main__":
+    # Ask the user for the ref to use
+    ref1, ref2, ref3, my_sheetname = ask_for_refname()
+    # Load the data
+    data = load_data(ref1, ref2, ref3)
+    # Calculate the average capacity
+    averaged_data = calculate_mean_cap(data)
+    # Append the result to the Excel file
+    append_to_capref(averaged_data, my_sheetname)
--- year_selection.py
+++ year_selection.py
@@ -1,98 +1,78 @@
 import pandas as pd
 import os
-from my_utils import completion_sound
-from time import sleep
+from my_utils import completion_sound, print_magenta
+import time 
 import winsound
-if __name__ == "__main__":
-    import multiprocessing
-    import time
+import multiprocessing
+from halfstepped_ref_sheet import load_data, calculate_mean_cap, append_to_capref, ask_for_refname
 
-    # profile_analysis is now a lot faster, so skipping it is not necessary anymore
-    #ask user whether to start from the beginning or from a specific step
-    #print("Press enter to start from the beginning")
-    #print("Enter a number between 1-3 to start at that step:")
-    #print("1. Start at profile analysis")
-    #print("2. Start at figure generation")
-    #print("3. Start at fingerprint matching")
-    #choice = input("Enter your choice: ")
-    choice = 1
+if __name__ == "__main__":
 
     sheets = pd.ExcelFile("input\\cap_ref.xlsx").sheet_names
-    # make sheet_name the name of the sheet that starts with "ref" and has the highest number after it
-    sheet_name = "ref" + str(max([int(i[3:]) for i in sheets if i.startswith("ref")]))
+    # make sheet_name the name of the left-most sheet that starts with "ref"
+    sheet_name = [sheet for sheet in sheets if sheet.startswith("ref")][0]
     ref_folder = sheet_name
-    # This is the latest ref_folder from the cap_ref.xlsx file
     print("The latest ref in ref_cap.xlsx is: " + ref_folder)
 
     ref_folders = []
     for folder in os.listdir("PickleJar"):
         if folder.startswith("ref"):
             ref_folders.append(folder)
-    ref_folders.sort(key=lambda x: int(x[3:]))
+    # pick the ref_folder that was most recently modified
+    ref_folders.sort(key=lambda x: os.path.getmtime(os.path.join("PickleJar", x)))
     ref_folder = ref_folders[-1]
     print("The latest ref in the PickleJar is: " + ref_folder)
-    # ask the user for the ref_folder to use
-    ref_folder_input = input("Enter the ref_folder to use: ")
-    if ref_folder_input == "":
-        ref_folder = ref_folder
-        print("Using ref_folder from PickleJar: " + ref_folder)
-        #elif numerical
-    elif ref_folder_input.isnumeric():
-        ref_folder = "ref" + ref_folder_input
-        print("Setting ref_folder to: " + ref_folder)
-    else:
-        ref_folder = ref_folder_input
-        print("Setting ref_folder to: " + ref_folder)
-
-
-    if choice == "":
-        choice = 1
-    else:
-        choice = int(choice)
-    if choice == 1:
-        print("Starting at profile analysis with ref_folder: " + ref_folder)
-    elif choice == 2:
-        print("Starting at figure generation with ref_folder: " + ref_folder)
-    elif choice == 3:
-        print("Starting at fingerprint matching")
-    else:
-        print("Invalid choice")
+    
+    #ask the user whether to use halfstepped (avg of two latest) or fullstepped (only most recent) capacities
+    while True:
+        halfstepped = input("Use halfstepped capacities? (y/n): ")
+        if halfstepped.lower() == "y" or halfstepped.lower() == "yes":
+            ref1, ref2, ref3, my_sheetname = ask_for_refname()
+            data = load_data(ref1, ref2, ref3)
+            avg_capacity = calculate_mean_cap(data)
+            append_to_capref(avg_capacity, my_sheetname)
+            print("Added sheet " + my_sheetname + " to cap_ref.xlsx")
+            #else: use the most recent ref_folder
+            ref_folder = my_sheetname
+            print("Setting ref_folder to: " + ref_folder)
+            break
+        elif halfstepped.lower() == "n" or halfstepped.lower() == "no":
+            # ask the user for the ref_folder to use
+            ref_folder_input = input("Enter the ref_folder to use: ")
+            if ref_folder_input == "":
+                ref_folder = ref_folder
+                print("Using ref_folder from PickleJar: " + ref_folder)
+                #elif numerical
+            elif ref_folder_input.isnumeric():
+                ref_folder = "ref" + ref_folder_input
+                print("Setting ref_folder to: " + ref_folder)
+            else:
+                ref_folder = ref_folder_input
+                print("Setting ref_folder to: " + ref_folder)
+            break
+        else:
+            #print a description of what halfstepped means
+            print_magenta("\nHalfstepped capacities means that the capacities are averaged from the two (or three) latest refs in cap_ref.xlsx")
+            print_magenta(" This should make the convergence more likely to succeed, but it MIGHT require more interations.\n\n")
     
     #Step 1
-    if choice==1: 
-        import profile_analysis
-        all_cap, VRE_groups, VRE_tech, VRE_tech_dict, VRE_tech_name_dict, years, reseamed_years, sites, region_name, \
-            regions, non_traditional_load, filenames, profile_keys, capacity_keys, fig_path, pickle_path, electrified_heat_demand \
-            = profile_analysis.initiate_parameters(ref_folder)
-        profile_analysis.separate_years(years, VRE_tech, VRE_tech_name_dict, filenames, profile_keys, capacity_keys,
-                                        fig_path, regions, VRE_groups, sites, non_traditional_load, electrified_heat_demand,
-                                        pickle_path, make_profiles=False, make_figure=True)
-        profile_analysis.combined_years(years, VRE_tech, VRE_tech_name_dict, filenames, profile_keys, regions,
-                   VRE_groups, sites, non_traditional_load, electrified_heat_demand, pickle_path,)
-        #combined_years(range(1980,1982))
-        profile_analysis.remake_profile_seam(pickle_path, electrified_heat_demand, non_traditional_load, make_profiles=False)
-        #plot_reseamed_years(range(1980,2020))
+    import profile_analysis
+    all_cap, VRE_groups, VRE_tech, VRE_tech_dict, VRE_tech_name_dict, years, reseamed_years, sites, region_name, \
+        regions, non_traditional_load, filenames, profile_keys, capacity_keys, fig_path, pickle_path, electrified_heat_demand \
+        = profile_analysis.initiate_parameters(ref_folder)
+    profile_analysis.separate_years(years, VRE_tech, VRE_tech_name_dict, filenames, profile_keys, capacity_keys,
+                                    fig_path, regions, VRE_groups, sites, non_traditional_load, electrified_heat_demand,
+                                    pickle_path, make_profiles=False, make_figure=True)
+    profile_analysis.combined_years(years, VRE_tech, VRE_tech_name_dict, filenames, profile_keys, regions,
+                VRE_groups, sites, non_traditional_load, electrified_heat_demand, pickle_path,)
+    #combined_years(range(1980,1982))
+    profile_analysis.remake_profile_seam(pickle_path, electrified_heat_demand, non_traditional_load, make_profiles=False)
+    #plot_reseamed_years(range(1980,2020))
 
     #Step 2
-    if choice<=2:
-        import figure_CFD
-        multiprocessing.freeze_support()
-        #profile_analysis.initiate()
-        figure_CFD.initiate(ref_folder)
-        completion_sound()
-        time.sleep(3)
-
-    # run the julia script by using the following powershell command: julia --threads 60 fingerprintmatching.jl
-    #Step 3
-    if choice<=3:
-        print("Better to run confirm the CFD plots and then run julia from the terminal..")
-        exit()
-        import subprocess
-        script_path = "fingerprintmatching.jl"
-        command = ["julia", "--threads", "60", script_path]
-
-        try:
-            subprocess.run(command, check=True)
-            print("Julia script executed successfully.")
-        except subprocess.CalledProcessError as e:
-            print(f"An error occurred while running the Julia script: {e}")
+    import figure_CFD
+    multiprocessing.freeze_support()
+    #profile_analysis.initiate()
+    figure_CFD.initiate(ref_folder)
+    completion_sound()
+    time.sleep(3)
