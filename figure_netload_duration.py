0a05c6a61a1bce3d9e89050876f7031e8354bc1e
Xhaf <jonathan.ullmark@hotmail.com>
2023-10-09 17:09:05 +0200

    Create figure_netload_duration.py

---
+++ figure_netload_duration.py
@@ -0,0 +1,4 @@
+# Gather the net load profiles of all years
+# Sort them to get the net load duration curves
+# Create an average net load duration curve
+# Plot the average net load duration curve along with 2012 and 2016-2017
